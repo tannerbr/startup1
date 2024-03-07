f2078f3794177126ffa6e9ee4ccb337d25a0d958
Tanner B <tbriggs0@student.byu.edu>
2024-03-07 11:47:34 -0700

    changes to loadTalks.js

--- public/loadTalks.js
+++ public/loadTalks.js
@@ -1,47 +1,72 @@
 // Change for service 
 async function loadTalks() {
-  let talks = [];
+  let talksList = [];
   try {
-    // Get the latest high scores from the service
+    // Get the latest high talks from the service
     const response = await fetch('/api/talks');
-    scores = await response.json();
-
-    // Save the scores in case we go offline in the future
-    localStorage.setItem('talks', JSON.stringify(talks));
+    talksList = await response.json();
+    // Save the talks in case go offline in the future
+    localStorage.setItem('talks', JSON.stringify(talksList));
   } catch {
-    // If there was an error then just use the last saved scores
+    // If error use the last saved talks
     const talksText = localStorage.getItem('talks');
     if (talksText) {
-      talks = JSON.parse(talksText);
+      talksList = JSON.parse(talksText);
     }
   }
-
-  displayScores(talks);
+  displayTalks(talksList);
 }
-
-function displayScores(talks) {
+function displayTalks(talksList) {
   const tableBodyEl = document.querySelector('#talks');
-
-  if (talks.length) {
-    // Update the DOM with the scores
-    for (const [i, score] of scores.entries()) {
+  if (talksList.length) {
+    // Update the DOM with talks
+    for (const [i, talk] of talksList.entries()) {
       const nameTdEl = document.createElement('td');
       const linkTdEl = document.createElement('td');
 
       positionTdEl.textContent = i + 1;
-      nameTdEl.textContent = score.name;
-      scoreTdEl.textContent = score.score;
-      dateTdEl.textContent = score.date;
+      nameTdEl.textContent = talk.talkTitle;
+      linkTdEl.textContent = talk.talkLink;
 
       const rowEl = document.createElement('tr');
       rowEl.appendChild(nameTdEl);
       rowEl.appendChild(linkTdEl);
-
       tableBodyEl.appendChild(rowEl);
+
     }
   } else {
     tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to suggest a talk</td></tr>';
   }
 }
-
 loadTalks();
+
+
+// old code before service
+// function loadTalks() {
+//   let talksList = [];
+//   const talkTitle = localStorage.getItem('talks');
+//   if (talkTitle) {
+//     talksList = JSON.parse(talkTitle);
+//   }
+
+//   const tableBodyEl = document.querySelector('#talks');
+
+//   if (talksList.length) {
+//     for (const [i, talk] of talksList.entries()) {
+//       const nameTdEl = document.createElement('td');
+//       const linkTdEl = document.createElement('td');
+
+//       nameTdEl.textContent = talk.talkTitle;
+//       linkTdEl.textContent = talk.talkLink;  
+
+//       const rowEl = document.createElement('tr');
+//       rowEl.appendChild(nameTdEl);
+//       rowEl.appendChild(linkTdEl);
+//       tableBodyEl.appendChild(rowEl);
+//     }
+//   } else {
+//     tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to suggest a talk</td></tr>';
+//   }
+// }
+
+// loadTalks();
