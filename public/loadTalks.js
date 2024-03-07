d0eff9f63bb85b2f63005f3e03291a9598c4fdf5
Tanner B <tbriggs0@student.byu.edu>
2024-03-07 11:33:06 -0700

    changes to loadTalks for service

--- public/loadTalks.js
+++ public/loadTalks.js
@@ -1,23 +1,42 @@
-function loadTalks() {
-  let talksList = [];
-  const talkTitle = localStorage.getItem('talks');
-  if (talkTitle) {
-    talksList = JSON.parse(talkTitle);
+// Change for service 
+async function loadTalks() {
+  let talks = [];
+  try {
+    // Get the latest high scores from the service
+    const response = await fetch('/api/talks');
+    scores = await response.json();
+
+    // Save the scores in case we go offline in the future
+    localStorage.setItem('talks', JSON.stringify(talks));
+  } catch {
+    // If there was an error then just use the last saved scores
+    const talksText = localStorage.getItem('talks');
+    if (talksText) {
+      talks = JSON.parse(talksText);
+    }
   }
 
+  displayScores(talks);
+}
+
+function displayScores(talks) {
   const tableBodyEl = document.querySelector('#talks');
 
-  if (talksList.length) {
-    for (const [i, talk] of talksList.entries()) {
+  if (talks.length) {
+    // Update the DOM with the scores
+    for (const [i, score] of scores.entries()) {
       const nameTdEl = document.createElement('td');
       const linkTdEl = document.createElement('td');
 
-      nameTdEl.textContent = talk.talkTitle;
-      linkTdEl.textContent = talk.talkLink;  
+      positionTdEl.textContent = i + 1;
+      nameTdEl.textContent = score.name;
+      scoreTdEl.textContent = score.score;
+      dateTdEl.textContent = score.date;
 
       const rowEl = document.createElement('tr');
       rowEl.appendChild(nameTdEl);
       rowEl.appendChild(linkTdEl);
+
       tableBodyEl.appendChild(rowEl);
     }
   } else {
