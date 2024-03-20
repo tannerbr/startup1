3bb5bc44f7eda0bc04a1ba90130fa6b6fc7b6b62
Tanner B <tbriggs0@student.byu.edu>
2024-03-20 15:45:42 -0600

    changes to login/logout

--- database.js
+++ database.js
@@ -4,7 +4,8 @@ const config = require('./dbConfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('startup');
-const talkCollection = db.collection('talk');
+const userCollection = db.collection('user');
+const scoreCollection = db.collection('talk');
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -14,6 +15,28 @@ const talkCollection = db.collection('talk');
   console.log(`Unable to connect to database with ${url} because ${ex.message}`);
   process.exit(1);
 });
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
 
 async function addTalk(talk) {
   const result = await talkCollection.insertOne(talk);
