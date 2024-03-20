527f2d6e339cf074c255876af8b527f0293a387a
Tanner B <tbriggs0@student.byu.edu>
2024-03-20 16:04:14 -0600

    changes to login and database

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostna
 const client = new MongoClient(url);
 const db = client.db('startup');
 const userCollection = db.collection('user');
-const scoreCollection = db.collection('talk');
+const talkCollection = db.collection('talk');
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -48,4 +48,10 @@ function getTalk() {
   return cursor.toArray();
 }
 
-module.exports = { addTalk, getTalk };
+module.exports = { 
+  addTalk, 
+  getTalk, 
+  getUser, 
+  getUserByToken, 
+  createUser 
+};
