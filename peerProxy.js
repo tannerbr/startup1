af9c4cc9945ca1ab7983c18b90b60c9f30bbdc90
Tanner B <tbriggs0@student.byu.edu>
2024-03-27 11:03:32 -0600

    websocket working displaying messages

--- peerProxy.js
+++ peerProxy.js
@@ -23,9 +23,7 @@ function peerProxy(httpServer) {
     // Forward messages to everyone except the sender
     ws.on('message', function message(data) {
       connections.forEach((c) => {
-        if (c.id !== connection.id) {
           c.ws.send(data);
-        }
       });
     });
 
--- public/likeButton.js
+++ public/likeButton.js
@@ -8,34 +8,37 @@ var likeCountInt = parseInt(likeCount) || 0;
 var userClicked = localStorage.getItem(userClicked);
 const likeButton = document.querySelector('#likeButton');
 var showLikeCount = document.getElementById('showLikeCount');
+var userName = localStorage.getItem('userName');
 var socket;
 
 // Event messages
 const likeEvent = 'likeEvent';
 
 function configureWebSocket() {
+    console.log("in configWS")
     const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
     socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
     socket.onopen = (event) => {
+        console.log("in onopen");
         displayMsg('system', 'talk', 'connected');
     };
-    this.socket.onclose = (event) => {
-        displayMsg('system', 'talk', 'disconnected');
+    socket.onclose = (event) => {        displayMsg('system', 'talk', 'disconnected');
     };
-    this.socket.onmessage = async (event) => {
+    socket.onmessage = async (event) => {
+        console.log("talk liked");
         const msg = JSON.parse(await event.data.text());
-        if (msg.type === TalkEndEvent) {
         displayMsg('user', msg.from, `liked this talk`);
-        } else if (msg.type === TalkStartEvent) {
-        displayMsg('user', msg.from, `started watching`);
-        }
+        // if (msg.type === TalkStartEvent) {
+        // displayMsg('user', msg.from, `started watching`);
+        // }
     };
     return socket;
 }
-
+configureWebSocket();
 
 
 function displayMsg(cls, from, msg) {
+    console.log("in display msg");
     const chatText = document.querySelector('#user-messages');
     chatText.innerHTML =
       `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
@@ -48,10 +51,9 @@ function displayMsg(cls, from, msg) {
       type: type,
       value: value,
     };
-    this.socket.send(JSON.stringify(event));
+    socket.send(JSON.stringify(event));
   }
 
-let wSocket = configureWebSocket();
 
 async function clickFunc(event){
     //event.stopPropagation();
